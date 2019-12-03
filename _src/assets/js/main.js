e75911006e6d826414129e68011f62ffa5406a7a
srojasz <srojasz@gmail.com>
2019-12-03 10:39:12 +0100

    Add selected favs to show

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -15,6 +15,10 @@ const showsList = document.querySelector(".js-shows__list");
 
 const favsList = document.querySelector(".js-favs__list");
 
+const showItems = document.querySelectorAll(".js-show__item");
+
+const resetButton = document.querySelector(".reset__btn");
+
 // Fetch que se ejecuta cuando hagamos click en buscar
 
 function getServerData(event) {
@@ -45,14 +49,24 @@ function paintShows() {
     let showId = shows[i].show.id;
     let showImage = shows[i].show.image;
 
+    const indexShow = favs.findIndex(function(fav) {
+      return fav.show.id === showId;
+    });
+    const isFav = indexShow !== -1;
+
     if (showImage === null) {
       showImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
     } else {
       showImage = showImage.medium;
     }
 
-    htmlCode += `<li class="js-show__item" id=${showId}>`;
-    htmlCode += `<h2>${showName}</h2>`;
+    if (isFav) {
+      htmlCode += `<li class="show__item js-show__item show__item__fav" id=${showId}>`;
+    } else {
+      htmlCode += `<li class="show__item js-show__item" id=${showId}>`;
+    }
+
+    htmlCode += `<h2 class="show__item__title">${showName}</h2>`;
     htmlCode += `<img src="${showImage}"/>`;
     htmlCode += "</li>";
   }
@@ -67,7 +81,7 @@ function listenShows() {
   }
 }
 
-// Añadir favorito al array (objeto completo)
+// Añadir favorito al array (objeto completo) y sacarlo cuando esté dentro
 
 function addFav(ev) {
   const clickedId = parseInt(ev.currentTarget.id);
@@ -95,39 +109,17 @@ function addFav(ev) {
     console.log(favs);
   }
 
+  paintShows();
+  listenShows();
   paintFavs();
   setFavData();
+
+  // const closeButtons = document.querySelectorAll(".js-fav__close");
+  // for (const closeButton of closeButtons) {
+  //   console.log("holi");
+  //   closeButton.addEventListener("click", addFav);
+  // }
 }
-
-// function addFav(ev) {
-//   const clickedId = parseInt(ev.currentTarget.id);
-
-//   const clickedShow = shows.find(function(show) {
-//     if (show.show.id === clickedId) {
-//       return true;
-//     }
-//   });
-
-//   const clickedFav = favs.find(function(fav) {
-//     if (fav.show.id === clickedId) {
-//       return true;
-//     }
-//   });
-
-//   if (clickedFav) {
-//     favs.filter(function(fav) {
-//       if (fav.show.id === clickedId) {
-//         return false;
-//       } else {
-//         return true;
-//       }
-//     });
-//   }
-
-// }
-//   paintFavs();
-//   setFavData();
-// }
 
 // Pintar favoritos
 
@@ -147,7 +139,7 @@ function paintFavs() {
     htmlCode += `<li class="fav__item js-fav__item" id=${favId}>`;
     htmlCode += `<div class="fav__item__header">`;
     htmlCode += `<h2 class="fav__title">${favName}</h2>`;
-    htmlCode += `<p class="fav__close js-fav__close">X</p>`;
+    htmlCode += `<div class="fav__close js-fav__close">X</div>`;
     htmlCode += `</div>`;
     htmlCode += `<img class="fav__img" src="${favImage}"/>`;
     htmlCode += "</li>";
@@ -169,11 +161,12 @@ function getFavData() {
   }
 }
 
-// function listenFavs() {
-//   for (const fav of favs) {
-//     fav.addEventListener("click", setFavData);
-//   }
-// }
+function resetFavs(ev) {
+  ev.preventDefault();
+  favs = [];
+  paintFavs();
+}
+resetButton.addEventListener("click", resetFavs);
 
 // getServerData();
 getFavData();
